684b3e751292ac01a8094e66ff5f7309666660ff
wangb <wangbin02@qianxin.com>
2019-12-05 12:58:26 +0800

    display select shape

--- build-2d-physics-engine/src/main.ts
+++ build-2d-physics-engine/src/main.ts
@@ -4,5 +4,9 @@ import { addRect, addCircle } from './control'
 const engine = new Engine(document.getElementById('canvas') as any)
 document.getElementById('add-rect')?.addEventListener('click', () => addRect(engine))
 document.getElementById('add-circle')?.addEventListener('click', () => addCircle(engine))
+document.getElementById('select-shape')?.addEventListener('change', (e) => {
+  const value = (e.currentTarget as any).value
+  engine.selectShape = value * 1
+})
 
 engine.render()
