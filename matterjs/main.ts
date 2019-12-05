20d101730a6277557188448db8591f7de53392c6
wangb <wangbin02@qianxin.com>
2019-12-05 10:11:38 +0800

    update matterjs

--- matterjs/main.ts
+++ matterjs/main.ts
@@ -1,17 +1,30 @@
-import { Engine, Render, World, Bodies } from 'matter-js'
+import { GUI } from 'dat.gui'
+import { Engine, Render, World, Bodies, Mouse, MouseConstraint } from 'matter-js'
 
 const engine = Engine.create()
 const render = Render.create({
   element: document.body,
   engine,
 })
+const world = engine.world
+const mouse = Mouse.create(render.canvas)
+const mouseConstaint = MouseConstraint.create(engine, {
+  mouse, constraint: {
+    stiffness: 0.98,
+    render: { visible: false }
+  } as any
+})
+
+const gui = new GUI()
 
 const boxA = Bodies.rectangle(400, 200, 80, 80)
 const boxB = Bodies.rectangle(450, 50, 80, 80)
+const ball = Bodies.circle(420, 100, 20)
 const bar = Bodies.rectangle(420, 300, 50, 10, { isStatic: true })
 const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true })
 
-World.add(engine.world, [boxA, boxB, bar, ground])
+World.add(world, [boxA, boxB, ball, bar, ground])
+World.add(world, mouseConstaint)
 
 Engine.run(engine)
 Render.run(render)
